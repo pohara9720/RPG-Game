b2e71e5aa3584678ba6fd364e83fbbdd009981ab
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-04-30 19:22:48 -0700

    429

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+var fighter=["Mcgregor","Cerone","Silva","Jones"];
+
+
+	$("#cmb").on("click", function(){
+	$("#enemyQue").append("#jjb, #asb,#dcb");
+});
+
+
+
+
+
+// add images and dynamic words inside figher buttons
+// after selecting character send other charcters to abailable opponents 
+// after selecting opponent you can fight them
+// add crystal code to specific fighters (add attack powers and counter powers)
+// activitace button
+// loop to pick new fighter if defeated or end game 
+
+
