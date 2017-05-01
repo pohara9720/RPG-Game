0c5c9aaf15597ea182709357ca9049e7996fa0ab
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-04-30 20:11:35 -0700

    430

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,20 @@
 
 var fighter=["Mcgregor","Cerone","Silva","Jones"];
 
+// //
+// window.onload = function() {
+//     if (window.jQuery) {  
+//         // jQuery is loaded  
+//         alert("Yeah!");
+//     } else {
+//         // jQuery is not loaded
+//         alert("Doesn't Work");
+//     }
+// }
 
-	$("#cmb").on("click", function(){
-	$("#enemyQue").append("#jjb, #asb,#dcb");
-});
+// 	$("#cmb").on("click", function(){
+// 	$("#enemyQue").append("#jjb, #asb,#dcb");
+// });
 
 
 
