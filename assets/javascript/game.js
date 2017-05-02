9cbaa8e9a403ea449fba9bb8958fe8f4e07108ce
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-05-02 01:08:45 -0700

    51

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,126 @@
-$(document).ready(function(){
-var conor = document.getElementById("cmb");
-var don = document.getElementById("dcb");
-var jon = document.getElementById("jjb");
-var andy = document.getElementById("asb");
-var octagon=true;
-var fighters=["Conor", "Donald", "Jon", "Anderson"];
+$(document).ready(function() {
+	// clean up variables
+	// utilize jQuery i.e. var conor = $("#cmb");
+    var conor = document.getElementById("cmb");
+    var don = document.getElementById("dcb");
+    var jon = document.getElementById("jjb");
+    var andy = document.getElementById("asb");
+    var octagon = false;
+    var cFighters = [don, jon, andy];
+    var jFighters = [conor, andy, don];
+    var aFighters = [conor, jon, don];
+    var dFighters = [jon, andy, conor];
+    var allFighters = [conor, andy, jon, don];
+    fighter1 = "";
+    fighter2 = "";
+    fighter3 = "";
 
 
-/////////////////////////////////////picking a fighter///////////////////////////////
-function select(){
-	$("#cmb").on("click", function(){
- 	$("#enemyQue").append(jon,andy,don);
+
+    // /////////////////////////////////////conor///////////////////////////////
+    
+        $("#cmb").on("click", function() {  
+        if (octagon===false){              
+            $("#enemyQue").append(cFighters);
+            $("#charSel").append(conor);
+        }
+        else(octagon===true)
+        	$(don).on("click",function(){
+        		$("#Octagon").append(don);
+
+        	});
+        $(jon).on("click",function(){
+        		$("#Octagon").append(jon);
+
+        	});
+        $(andy).on("click",function(){
+        		$("#Octagon").append(andy);
+
+        	});
+        });
+    
+    // /////////////////////////////////////andy///////////////////////////////
+
+        $("#asb").on("click", function() {
+        	if (octagon===false){ 
+            $("#enemyQue").append(aFighters);
+            $("#charSel").append(andy);
+        }
+          else(octagon===true)
+        	$(don).on("click",function(){
+        		$("#Octagon").append(don);
+
+        	});
+        $(jon).on("click",function(){
+        		$("#Octagon").append(jon);
+
+        	});
+        $(conor).on("click",function(){
+        		$("#Octagon").append(conor);
+
+        	});
+        });
+    
+    //  /////////////////////////////////////don///////////////////////////////
+
+        $("#dcb").on("click", function() {
+        	if (octagon===false){ 
+            $("#enemyQue").append(dFighters);
+            $("#charSel").append(don);
+        }
+          else(octagon===true)
+        	$(conor).on("click",function(){
+        		$("#Octagon").append(conor);
+
+        	});
+        $(jon).on("click",function(){
+        		$("#Octagon").append(jon);
+
+        	});
+        $(andy).on("click",function(){
+        		$("#Octagon").append(andy);
+
+        	});
+        });
+    
+  /////////////////////////////////jon///////////////////////////////
+
+        $("#jjb").on("click", function() {
+        	if (octagon===false){ 
+            $("#enemyQue").append(jFighters);
+            $("#charSel").append(jon);
+        }
+          else(octagon===true)
+        	$(conor).on("click",function(){
+        		$("#Octagon").append(conor);
+
+        	});
+        $(don).on("click",function(){
+        		$("#Octagon").append(don);
+
+        	});
+        $(andy).on("click",function(){
+        		$("#Octagon").append(andy);
+
+        	});
+        });
+    
+
 
 });
-	$("#asb").on("click", function(){
- 	$("#enemyQue").append(jon,conor,don);
-});
-	$("#dcb").on("click", function(){
- 	$("#enemyQue").append(jon,andy,conor);
-});
-	$("#jjb").on("click", function(){
- 	$("#enemyQue").append(conor,andy,don);
-});
 
-};
-
-$(".fighter").on("click", function(){
-select();
-console.log("working");
-
-});
+    ///////////////////////////////////sending fighter to octagon///////////////////////////
 
 
-});
+
+
+    // $("enemyQue").on("click", function(){
+    // $("Octagon").append(".fighter");
+    // console.log("working2");
+
+    // });
+
+
 
 
 
@@ -43,6 +131,4 @@ console.log("working");
 // after selecting opponent you can fight them
 // add crystal code to specific fighters (add attack powers and counter powers)
 // activitace button
-// loop to pick new fighter if defeated or end game 
-
-
+// loop to pick new fighter if defeated or end game
